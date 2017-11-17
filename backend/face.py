ca109838e1662d468d38aba3859b47b4e7dce3ea
cheeyauk <cykee3@gmail.com>
2017-11-18 05:05:34 +0800

    json POST
    json POST

--- backend/face.py
+++ backend/face.py
@@ -8,6 +8,10 @@ Created on Sat Nov 18 01:24:07 2017
 
 from flask import Flask,request
 from PIL import Image
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
+
 
 app = Flask(__name__)
 
@@ -15,34 +19,16 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 @app.route("/img", methods=["POST"])
 def home():
-    img = Image.open(request.files['file'])
-    img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
-    return 'Success!'
+    content = request.get_json()
+    #img = Image.open(request.files['file'])
+    print(content,sys.stdout)
+    sys.stdout.flush()
+    #img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
+    return str(content)
     
 if __name__ == '__main__':
     app.run(debug = True)
