9b43382790842fe9e79a875e6b62a889915bde3c
cheeyauk <cykee3@gmail.com>
2017-11-18 10:08:18 +0800

    update latest cors

--- backend/face.py
+++ backend/face.py
@@ -8,16 +8,18 @@ Created on Sat Nov 18 01:24:07 2017
 
 from __future__ import print_function
 from flask import Flask,request
+from flask_cors import CORS,cross_origin
 from PIL import Image
 import sys
-<<<<<<< HEAD
+
 import logging
 from logging.handlers import RotatingFileHandler
 
-=======
->>>>>>> 7f776adf54daeee5ca7ed1475dd07cd61097cdbc
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app,resources={r"/*": {"origins": "*"}},allow_headers='*')
+
 print("initiated the app! With a name of:", __name__)
 
 @app.route('/')
@@ -29,9 +31,10 @@ def index():
     return "Hello, World!"
 
 app = Flask(__name__)
-<<<<<<< HEAD
+
 
 @app.route("/img", methods=["POST"])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def home():
     content = request.get_json()
     #img = Image.open(request.files['file'])
@@ -39,34 +42,7 @@ def home():
     sys.stdout.flush()
     #img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
     return str(content)
-=======
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit a empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-@app.route("/img", methods=["POST"])
-def home():
-    print('Hello world!', file=sys.stderr)
-    print('This standard output', file=sys.stdout)
-    app.logger.info('Test')
-    img = Image.open(request.files['file'])
-    img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
-    return 'Success!'
->>>>>>> 7f776adf54daeee5ca7ed1475dd07cd61097cdbc
     
 if __name__ == '__main__':
     app.run(debug = True)
