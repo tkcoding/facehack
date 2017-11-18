65c09992778444846191c7e88d41ee630ae54708
Darren Yim <darrenyxj@gmail.com>
2017-11-18 13:32:32 +0800

    add temporary list features

--- frontend/src/app/face2/face2.component.ts
+++ frontend/src/app/face2/face2.component.ts
@@ -1,56 +1,56 @@
+import { Rec } from './rec.model';
 import { Component, OnInit } from '@angular/core';
 import { ItemsService } from './../services/items.service';
 
 @Component({
-  selector: 'app-face2',
-  templateUrl: './face2.component.html',
-  styleUrls: ['./face2.component.scss']
+	selector: 'app-face2',
+	templateUrl: './face2.component.html',
+	styleUrls: ['./face2.component.scss']
 })
 export class Face2Component implements OnInit {
-  myHeaders: { [name: string]: any } = {
-    'Content-type': 'application/octet-stream',
-    'X-Access-Token': 'fGF1DfdEWCeTaAqAdEsHXY0MMuo92avXuOhL'
-  };
-  constructor(private itemService: ItemsService) { }
+	recs: Rec[];
 
-  ngOnInit() {
+	constructor(private itemService: ItemsService) {
+		this.recs = new Array<Rec>();
+		this.recs.push(new Rec("name1","abc1","abcd1","abcde1"));
+		this.recs.push(new Rec("name2","abc2","abcd2","abcde2"));
+	 }
 
-  }
+	ngOnInit() {
 
-  dostuff2() {
+	}
 
-    var fileInput = document.getElementById("input1") as HTMLInputElement;    
-    var files = fileInput.files;    
+	dostuff2() {
+		var fileInput = document.getElementById("input1") as HTMLInputElement;
+		var files = fileInput.files;
 
-    var xhr = new XMLHttpRequest();
-    var result
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        result = xhr.responseText;
-        console.log('success!!')
-        this.postData(result);
-      }
-    }
+		var xhr = new XMLHttpRequest();
+		var result
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				result = xhr.responseText;
+				console.log('success!!');
+				this.postData(result);
+			}
+		}
 
-    xhr.open("POST", "https://dev.sighthoundapi.com/v1/detections?type=face,person&faceOption=landmark,gender");
-    xhr.setRequestHeader("Content-type", "application/octet-stream");
-    xhr.setRequestHeader("X-Access-Token", "fGF1DfdEWCeTaAqAdEsHXY0MMuo92avXuOhL");
-    xhr.send(files[0]);
-  }// dostuff2 end
+		xhr.open("POST", "https://dev.sighthoundapi.com/v1/detections?type=face,person&faceOption=landmark,gender");
+		xhr.setRequestHeader("Content-type", "application/octet-stream");
+		xhr.setRequestHeader("X-Access-Token", "fGF1DfdEWCeTaAqAdEsHXY0MMuo92avXuOhL");
+		xhr.send(files[0]);
+	}// dostuff2 end
 
-  postData(data) {
-    let j = JSON.stringify(data);
-    
-    this.itemService.postFaceData(j).subscribe(
-      (item: any) => {
-        if (item != null) {
-          console.log(item);
-          // Add item to array
-          // this.items.push(item);
-        }
-      },
-      (error) => console.log(error)
-    );
-  }
+	postData(data) {
+		let j = JSON.stringify(data);
+
+		this.itemService.postFaceData(j).subscribe(
+			(item: any) => {
+				if (item != null) {
+					
+				}
+			},
+			(error) => console.log(error)
+		);
+	}
 
 }
---
+++ frontend/src/app/face2/rec.model.ts
@@ -0,0 +1,13 @@
+export class Rec {
+	public pname;
+	public productImage;
+	public description;
+	public recommendation;
+
+	constructor(pname, productImage, description, recommendation) {
+		this.pname = pname;
+		this.productImage = productImage;
+		this.description = description;
+		this.recommendation = recommendation;
+	}
+}
